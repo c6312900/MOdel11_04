7403df25b839f5ba9449d8d06d26af7e01ecc2a4
c6312900 <c6312900@gmail.com>
2017-06-06 19:52:15 +0800

    Model11_04

---
+++ MOdel11_01/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MOdel11_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Home
+        public ActionResult TempDataDemo()
+        {
+            ViewData["key1"] = DateTime.Now.ToString();
+            ViewBag.Key2= DateTime.Now.ToString();
+            TempData["key3"]= DateTime.Now.ToString();
+
+            return RedirectToAction("TempDataDemo2");
+        }
+
+        public ActionResult TempDataDemo2()
+        {
+            return View();
+        }
+
+        public ActionResult SessionDemo()
+        {
+            if (Session["count"] == null)
+                Session["count"] = 0;
+            else
+                Session["count"] = (int)Session["count"] + 1;
+
+            return View();
+        }
+
+
+    }
+}
